45b9fc4b93a7c57c0d1690d618353ef3ee4e862f
Netson <netsoncavina@gmail.com>
2022-01-13 15:03:15 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    title: "Noxus",
+    type: "Expansionist Empire",
+    mapUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Noxus is a powerful empire with a fearsome reputation. To those beyond its borders, Noxus is brutal, expansionist, and threatening, yet those who look beyond its warlike exterior see an unusually inclusive society, where the strengths and talents of its people are respected and cultivated.",
+    iconUrl:
+      "https://static.wikia.nocookie.net/leagueoflegends/images/3/38/Noxus_Crest_icon.png/revision/latest/scale-to-width-down/131?cb=20161117055828",
+    imageUrl:
+      "https://static.wikia.nocookie.net/leagueoflegends/images/8/82/Noxus_The_Immortal_Bastion_01.jpg/revision/latest/scale-to-width-down/1000?cb=20181118145414",
+  },
+];
