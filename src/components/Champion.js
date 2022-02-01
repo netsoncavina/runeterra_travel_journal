228edb40d5d53384b63188be80183cddf461744b
Netson <netsoncavina@gmail.com>
2022-02-01 19:08:11 -0300

    Added champions nicknames

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,9 +1,20 @@
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css"; // optional
+
 export default function Champion(props) {
   const srcUrl = `images/champion/${props.name}.png`;
   return (
     <div className="card--champion--list">
-      <img className="card--champion--icon" src={srcUrl} />
-      <h5>{props.name}</h5>
+      <Tippy
+        content={
+          <a>
+            {props.name} : {props.nick}
+          </a>
+        }
+      >
+        <img className="card--champion--icon" src={srcUrl} />
+      </Tippy>
+      {/* <h5>{props.name}</h5> */}
     </div>
   );
 }
