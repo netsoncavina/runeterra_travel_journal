81d0f43d237a00eb243205a968aeeb887ed13997
Netson <netsoncavina@gmail.com>
2022-02-24 18:29:41 -0300

    removed commented lines

--- src/index.js
+++ src/index.js
@@ -3,37 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// Translation
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
-
 import reportWebVitals from "./reportWebVitals";
-
-// i18n
-//   .use(initReactI18next) // passes i18n down to react-i18next
-//   .use(LanguageDetector)
-//   .use(HttpApi)
-//   .init({
-//     fallbackLng: "en",
-//     detection: {
-//       order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
-//       caches: ["cookie"],
-//     },
-//     backend: {
-//       loadPath: "/assets/locales/{{lng}}/translation.json",
-//     },
-//     react: {
-//       useSuspense: false,
-//     },
-//   });
-
-// function App() {
-//   const { t } = useTranslation();
-
-//   return <h2>{t("noxus")}</h2>;
-// }
 
 ReactDOM.render(
   <React.StrictMode>
